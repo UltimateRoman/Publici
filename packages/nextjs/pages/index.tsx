72cf3eb0a31cb9cf9d646015ec613ffdb4822e89
UltimateRoman <59477486+UltimateRoman@users.noreply.github.com>
2024-06-02 21:35:30 +0700

    Updated frontend

--- packages/nextjs/pages/index.tsx
+++ packages/nextjs/pages/index.tsx
@@ -7,19 +7,28 @@ import { useAccountBalance, useScaffoldContractRead, useScaffoldContractWrite } 
 const Home: NextPage = () => {
   const [balances, setBalances] = useState<{ [key: string]: string }>({});
   const { address } = useAccount();
-  const [imageLink, setImageLink] = useState<string[]>([]);
   const { balance } = useAccountBalance(address);
+  const [imageLink, setImageLink] = useState<string>("");
+  const [generatedFlag, setGeneratedFlag] = useState<boolean>(false);
+
+  const { data: usdtBalance } = useScaffoldContractRead({
+    contractName: "USDT",
+    functionName: "balanceOf",
+    args: [address],
+  });
 
   const { writeAsync: writeManager } = useScaffoldContractWrite({
     contractName: "Manager",
     functionName: "queryAgent",
-    args: [`I have ${balance} amount of GAL in my wallet, Generate me an Image to depict my Portfolio`],
+    args: [`Generate an image that represents a crypto portfolio which depicts the crypto holdings of the account holder in ETH
+      and USDT tokens. The image should be generated in a way that it can be minted as an NFT. The account has a balance of ${balance} 
+      ETH and ${usdtBalance} USDT. Depict the balances along with their total USD value`],
   });
 
   const { writeAsync: mintNFT } = useScaffoldContractWrite({
     contractName: "Manager",
     functionName: "mintNFT",
-    args: [imageLink[0]],
+    args: [`{"name": "Publici Portfolio Snapshot", "image": "${imageLink}", "description": "A snapshot of the current portfolio holdings of the account holder"}`],
   });
 
   const { data: messageHistory } = useScaffoldContractRead({
@@ -35,37 +44,43 @@ const Home: NextPage = () => {
   }, [balance]);
 
   useEffect(() => {
-    if (messageHistory && Array.isArray(messageHistory)) {
+    if (messageHistory && Array.isArray(messageHistory) && generatedFlag === true) {
       const links = messageHistory.filter(item => item.includes("http")).map(item => item.replace(/"/g, ""));
-      setImageLink(links);
+      setImageLink(links[links.length - 1]);
     }
   }, [messageHistory]);
 
   return (
     <>
       <MetaHeader />
-      <div className="flex items-center flex-col flex-grow pt-10">
+      <div className="flex items-center flex-col flex-grow pt-2">
         <div className="flex justify-center items-center h-screen">
           <div className="bg-white shadow-lg rounded-lg p-8 w-96">
             <div className="text-center mb-4">
               <h2 className="text-xl font-bold">Portfolio</h2>
               <div className="mt-4">
                 <div className="mt-4">
-                  {Object.entries(balances).map(([token, amount]) => (
-                    <div key={token} className="flex justify-between border-b py-2">
-                      <span>{token}:</span>
-                      <span>{amount}</span>
-                    </div>
-                  ))}
+                    <div key="0" className="flex justify-between border-b py-2">
+                      <span>ETH: </span>
+                      <span>{balance}</span>
+                    </div> 
+                    {
+                      usdtBalance &&
+                      <div key="1" className="flex justify-between border-b py-2">
+                        <span>USDT: </span>
+                        <span>{(Number(usdtBalance) / 10**18).toString()}</span>
+                      </div>
+                    }
                 </div>
               </div>
             </div>
             <div className="text-center">
               <button
-                className="btn btn-primary mb-4"
+                className="btn btn-primary mb-4 text-white"
                 onClick={async () => {
                   try {
                     await writeManager();
+                    setGeneratedFlag(true);
                   } catch (error) {
                     console.error("Error generating image:", error);
                   }
@@ -74,15 +89,15 @@ const Home: NextPage = () => {
                 Generate
               </button>
               <div className="border rounded-lg p-4 mb-4 h-64 flex justify-center items-center overflow-hidden">
-                {imageLink.length > 0 ? (
-                  <img src={imageLink[0]} alt="Generated Image" className="max-w-full max-h-full object-contain" />
+                {imageLink != "" ? (
+                  <img src={imageLink} alt="Generated Image" className="max-w-full max-h-full object-contain" />
                 ) : (
-                  <span>[Image]</span>
+                  <span>[Generated Image]</span>
                 )}
               </div>
 
               <button
-                className="btn btn-secondary"
+                className="btn btn-secondary text-white"
                 onClick={async () => {
                   try {
                     await mintNFT();
